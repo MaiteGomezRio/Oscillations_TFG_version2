9d0f591db40816d5b4eef4d45ac9fcf4addb3d02
Maite Gomez del Rio <maite.gomezriovinuesa@usp.ceu.es>
2025-06-24 20:18:19 +0200

    Limpieza de archivos y actualización de corrector/processor

--- processing/corrector.py
+++ processing/corrector.py
@@ -1,11 +1,7 @@
-
+#processing/corrector.py
 
 import numpy as np
 from scipy.integrate import cumulative_trapezoid
-from utils.fft_utils2 import local_frequencies_with_fft
-from utils.filters import apply_savgol_filter
-import matplotlib.pyplot as plt
-import pandas as pd
 
 def build_xic(mz_array, intensity_array, rt_array, target_mz, mz_tol=0.1):
     """
@@ -50,8 +46,8 @@ def get_amplitude(target_mz, xic, rt_array, local_freqs, sampling_interval):
         
         period=int(1/(freq*sampling_interval))
         
-        if period<3: #para que sea al menos 3 si no es muy pequeña
-            continue
+        #if period<3: #para que sea al menos 3 si no es muy pequeña
+            #continue
         
         center=i*int(len(xic) / len(local_freqs))
         start=int(max(0, center-period/2))
@@ -59,10 +55,16 @@ def get_amplitude(target_mz, xic, rt_array, local_freqs, sampling_interval):
         
         
         window=xic[start:end]
-        local_amplitude=np.max(window)-np.min(window)
+        #local_amplitude=np.max(window)-np.min(window)
+        #local_amplitudes.append(local_amplitude)
+        
+        q25, q75 = np.percentile(window, [25, 75])
+        local_amplitude = (q75 - q25) / 2
         local_amplitudes.append(local_amplitude)
         
-    amplitude=np.median(local_amplitudes)/2
+        
+    #amplitude=np.median(local_amplitudes)/4
+    amplitude = np.percentile(local_amplitudes, 75)
     
     return amplitude
     
@@ -79,93 +81,6 @@ def generate_modulated_signal(amplitude, phase):
     modulated_signal = amplitude * np.sin(phase) 
     
     return modulated_signal
-
-def plot_all(rts, target_mz, xic, modulated_signal, residual_signal):
-    """
-    Plots the Xic, modulated signal and residual signal for an specific m/z in 2 different subplots 
-
-    Parameters
-    ----------
-    xic
-    modulated_signal
-    residual_signal
-
-    Returns
-    -------
-    None.
-
-    """
-    fig, axs = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
-
-    # First subplot: original and modulated signal
-    axs[0].plot(rts, xic, label='XIC original', linewidth=0.8, color='grey')
-    axs[0].plot(rts, modulated_signal, label='Modulated signal', linestyle='--', color='orange')
-    axs[0].set_ylabel("Intensity")
-    axs[0].set_title(f"XIC and Modulated Signal for m/z = {target_mz}")
-    axs[0].legend()
-    axs[0].grid(True)
-
-    # Second subplot: residual signal
-    axs[1].plot(rts, residual_signal, label='Residual signal', color='green', linewidth=0.8)
-    axs[1].set_xlabel("Retention time (s)")
-    axs[1].set_ylabel("Intensity")
-    axs[1].set_title("Residual Signal")
-    axs[1].legend()
-    axs[1].grid(True)
-    
-    #Para que tengan la misma escala
-    all_values = np.concatenate([xic, modulated_signal, residual_signal])
-    y_min, y_max = np.min(all_values), np.max(all_values)
-
-    # Aplicar la misma escala
-    axs[0].set_ylim(y_min, y_max)
-    axs[1].set_ylim(y_min, y_max)
-
-    plt.tight_layout()
-    plt.show()
-    
-def plot_modulated_signal(rts, target_mz, modulated_signal):
-    plt.figure(figsize=(10, 6))
-    plt.plot(rts, modulated_signal, label='Modulated signal', linestyle='--', color='orange')
-    plt.xlabel("Retention time (s)")
-    plt.ylabel("Intesity")
-    plt.title(f"Modulated signal for m/z = {target_mz}")
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-
-def plot_residual_signal(rt_array, target_mz, residual_signal):
-    """
-    Plots only the residual signal versus retention time.
-
-    Parameters
-    ----------
-    rt_array : array-like
-        Retention times.
-    residual_signal : array-like
-        Residual signal after modulation removal.
-    target_mz : float, optional
-        Target m/z for reference in the title.
-
-    Returns
-    -------
-    None
-    """
-    plt.figure(figsize=(10, 5))
-    plt.plot(rt_array[:40], residual_signal[:40], label='Residual signal', color='blue', linewidth=0.9)
-
-    plt.xlabel("Retention time (s)")
-    plt.ylabel("Residual intensity")
-    title = "Residual Signal"
-    if target_mz is not None:
-        title += f" for m/z = {target_mz}"
-    plt.title(title)
-    
-    plt.grid(True)
-    plt.tight_layout()
-    plt.legend()
-    plt.show()
     
 def correct_oscillations(rt_array, mz_array, intensity_array, phase_ref, local_freqs_ref, target_mz, window_size=70):
     """
@@ -192,24 +107,30 @@ def correct_oscillations(rt_array, mz_array, intensity_array, phase_ref, local_f
     #phase=apply_polynomial_regression(rt_array, rt_freqs, local_freqs)
     
     
+    #[DEBUG] PARA COMPROBAR QUE LA FRECUENCIA ES CTE
+    #print(f" FREQS FOR MZ: {target_mz} are: {local_freqs_ref}")
+    
+    
     # 3. Amplitude at each m/z
     amplitude=get_amplitude(target_mz, xic, rt_array, local_freqs_ref, sampling_interval)
     #print(f"Amplitude for m/z: {target_mz} is: {amplitude}")
     
     # 4. Creation of the modulated signal
-    modulated_signal=generate_modulated_signal(amplitude, phase_ref)
-    offset=np.median(xic)
-    #offset = apply_savgol_filter(xic, window_length=70, filter_order=2)
-    #modulated_signal+=offset
+    
     
     # 5. Computation of the residual/final signal
-    residual_signal=xic-np.median(modulated_signal)
-    #esidual_signal = np.where(modulated_signal > xic, modulated_signal - xic,
-                           #xic - modulated_signal)
+    
+    modulated_signal = generate_modulated_signal(amplitude, phase_ref)
+    #baseline = np.median(xic)
+    #modulated_signal = baseline + amplitude * np.sin(phase) señal centrada en la baseline de la xic
+    #residual_signal = xic - (modulated_signal - baseline)#le resto solo la amplitud con respecto a esa baseline
+    
+    #que viene a ser lo mismo que hacer esto: 
+    residual_signal = xic - modulated_signal
     #residual_signal_adjusted=np.clip(residual_signal, 0, None)
     
-    #[DEBUG]
     
+    #[DEBUG]
     """print("Mz: 65.1")
     if target_mz==65.1:
         residual_df = pd.DataFrame({
--- processing/processor.py
+++ processing/processor.py
@@ -4,83 +4,21 @@ import os
 import time
 import numpy as np
 import pandas as pd
-#from collections import Counter
-from processing.corrector_3 import correct_oscillations, plot_all, plot_modulated_signal, plot_residual_signal, build_xic, apply_polynomial_regression
-from utils.io_utils import convert_mzxml_2_mzml
-from validation.signal_validator2 import plot_corrected_signal_bpc, compare_bpc_values, compare_frequencies_over_rt,  build_variable_frequency_sine_and_plot_residual, verify_residual_signal, debug_correccion
-#from simulated_corrector2_tryFile import plot_signal, correct_oscillations_per_bin, detect_oscillating_mzs, plot_tic_comparison, export_tic_signals_to_csv, export_xic_signals_combined_csv
-import time
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-from processing.notch_filter import notch_filter, plot_xic_filtered
-from processing.wavelet_filter import correct_oscillations_wavelet, plot_original_and_residual
-from processing.gaussian_filter import apply_gaussian_filter
+from collections import defaultdict#used to extract all mzs with signal
+
+from processing.corrector_3 import correct_oscillations, build_xic, apply_polynomial_regression
+from utils.io_utils import convert_mzxml_2_mzml
+#from filtering.notch_filter import notch_filter, plot_xic_filtered
+#from filtering.wavelet_filter import correct_oscillations_wavelet, plot_original_and_residual
+#from filtering.gaussian_filter import apply_gaussian_filter
 from utils.fft_utils2 import local_frequencies_with_fft
+from validation.validator import plot_all, export_xic_signals_combined_csv
 
-def export_xic_signals_combined_csv(rts, xic_signals, modulated_signals, residual_signals, output_csv_path):
-    """
-    Exporta un único CSV con los XIC, señales moduladas y residuales para cada m/z.
-    """
-    df = pd.DataFrame({'RT': rts})
 
-    for target_mz in xic_signals:
-        mz_str = f"{target_mz:.4f}"
-        df[f"XIC_{mz_str}"] = xic_signals[target_mz]
-        df[f"Modulated_{mz_str}"] = modulated_signals[target_mz]
-        df[f"Residual_{mz_str}"] = residual_signals[target_mz]
-
-    # Aplicar formato español a todos los valores numéricos
-    df_formatted = df.map(
-        lambda x: f"{x:,.6f}".replace(",", "X").replace(".", ",").replace("X", ".")
-        if isinstance(x, (float, int)) else x
-    )
-
-    # Guardar con separador de columnas ';' (opcional pero común en CSVs en español)
-    df_formatted.to_csv(output_csv_path, index=False, sep=';')
-    print(f"Exportado CSV combinado a: {output_csv_path}")
     
-def plot_ms_experiment_3d(ms_experiment):
-    """
-    Grafica un experimento MS (MSExperiment) en 3D con RT, m/z, y intensidad, con color según la intensidad.
-    """
-    rts = []
-    mzs = []
-    intensities = []
-
-    for spec in ms_experiment:
-        rt = spec.getRT()
-        mz, inten = spec.get_peaks()
-
-        if len(mz) == 0:
-            continue  # ignorar espectros vacíos
-
-        rts.extend([rt] * len(mz))
-        mzs.extend(mz)
-        intensities.extend(inten)
-
-    rts = np.array(rts)
-    mzs = np.array(mzs)
-    intensities = np.array(intensities)
-
-    fig = plt.figure(figsize=(12, 6))
-    ax = fig.add_subplot(111, projection='3d')
-
-    # Crear el scatter plot con mapeo de color
-    sc = ax.scatter(rts, mzs, intensities, c=intensities, cmap='viridis', marker='o', s=5, alpha=0.8)
-
-    # Añadir colorbar
-    cbar = fig.colorbar(sc, ax=ax, pad=0.1)
-    cbar.set_label("Intensity")
-
-    ax.set_xlabel("Retention Time (s)")
-    ax.set_ylabel("m/z")
-    ax.set_zlabel("Intensity")
-    ax.set_title("3D MS Corrected Map (Color = Intensity)")
-
-    plt.tight_layout()
-    plt.show()
-    
-def correct_spectra(input_map, tic_original, residual_signal):
+def correct_spectra_with_tic(input_map, tic_original, residual_signal):
     """
     TIC
     
@@ -127,62 +65,7 @@ def correct_spectra(input_map, tic_original, residual_signal):
     
     return corrected_map
 
-def correct_spectra2(input_map, target_mz_list, rts, residual_signals, mz_tol=0.1):
-    """
-    MZ WITH MODULATED SIGNAL 
-
-    Parameters
-    ----------
-    input_map : TYPE
-        DESCRIPTION.
-    target_mz_list : TYPE
-        DESCRIPTION.
-    rts : TYPE
-        DESCRIPTION.
-    residual_signals : TYPE
-        DESCRIPTION.
-    mz_tol : TYPE, optional
-        DESCRIPTION. The default is 0.1.
-
-    Returns
-    -------
-    corrected_map : TYPE
-        DESCRIPTION.
-
-    """
-    # para mantener el input_map intacto
-    corrected_map = oms.MSExperiment()
-    
-    # Copiar espectros para no modificar input_map
-    for spec in input_map:
-        new_spec = oms.MSSpectrum(spec)  # constructor que copia los datos
-        corrected_map.addSpectrum(new_spec)
-
-    # Interpolar de señales residuales por m/z
-    interpoladores = {
-        target_mz: lambda rt, res=residual_signals[target_mz]: float(np.interp(rt, rts, res))
-        for target_mz in target_mz_list
-    }
-    
-    for spec in corrected_map:
-        
-        rt = spec.getRT()
-        mzs, intensities = spec.get_peaks()
-        intensities = np.array(intensities)
-
-        for i in range(len(mzs)):
-            for target_mz in target_mz_list:
-                if abs(mzs[i] - target_mz) <= mz_tol:
-                    residual = interpoladores[target_mz](rt)
-                    #print(f"Match at RT={rt:.2f}s | m/z={mzs[i]:.4f} ~ {target_mz:.4f} -> Intensity set to {residual:.2f}")
-                    intensities[i] = residual
-                    #print(f"Intensity at Mz: {i}    is: {intensities[i]:.2f}")
-
-        spec.set_peaks((mzs, intensities))
-
-    return corrected_map
-
-def correct_spectra3(input_map, target_mz_list, rts, residual_signals, mz_tol=0.1):
+def correct_spectra_gaussian_filter(input_map, target_mz_list, rts, residual_signals, mz_tol=0.1):
     """
     GAUSSIAN FILTER
     Applies correction to spectra. 
@@ -252,24 +135,120 @@ def correct_spectra3(input_map, target_mz_list, rts, residual_signals, mz_tol=0.
     
     return corrected_map
 
-def plot_xic_from_map(ms_map, target_mz, mz_tol=0.01):
-    rts = []
-    intensities = []
-    for spec in ms_map:
-        if spec.getMSLevel() != 1:
+def detect_oscillating_mzs(rts, mz_array, intensity_array, base_target_mzs, phase_ref, local_freqs_ref, intensity_threshold=100, osc_treshold=0.1, max_mzs_to_check=500):
+    detected_mzs=extract_detected_mzs(rts, mz_array, intensity_array)
+    auto_selected_mzs=[]
+    
+    for mz in detected_mzs:
+        if mz in base_target_mzs:
             continue
-        mzs, intens = spec.get_peaks()
-        rt = spec.getRT()
-        for mz, intensity in zip(mzs, intens):
-            if abs(mz - target_mz) <= mz_tol:
-                rts.append(rt)
-                intensities.append(intensity)
-                break
-    plt.plot(rts, intensities)
-    plt.xlabel("Retention Time (s)")
-    plt.ylabel(f"Intensity at {target_mz} m/z")
-    plt.title(f"XIC of {target_mz}")
-    plt.show()
+        try:
+            xic, modulated_signal, _=correct_oscillations(rts, mz_array, intensity_array, phase_ref, local_freqs_ref, mz)
+            intensity_importance=np.var(modulated_signal)/(np.var(xic)+1e-8)
+            if intensity_importance > osc_treshold:
+                auto_selected_mzs.append(mz)
+        except Exception as e:
+            print(f"[!] Error analyzing m/z {mz}:{e}")
+            
+        if len(auto_selected_mzs) >= max_mzs_to_check:
+            break #porque si no hay sobrecarga de memoria/CPU 
+    
+    return auto_selected_mzs
+        
+def correct_spectra(input_map, target_mz_list, rts, residual_signals, mz_tol=0.1, threshold=0.0012):
+    """
+    MZ WITH MODULATED SIGNAL 
+
+    Parameters
+    ----------
+    input_map : TYPE
+        DESCRIPTION.
+    target_mz_list : TYPE
+        DESCRIPTION.
+    rts : TYPE
+        DESCRIPTION.
+    residual_signals : TYPE
+        DESCRIPTION.
+    mz_tol : TYPE, optional
+        DESCRIPTION. The default is 0.1.
+
+    Returns
+    -------
+    corrected_map : TYPE
+        DESCRIPTION.
+
+    """
+    
+    # para mantener el input_map intacto
+    corrected_map = oms.MSExperiment()
+    
+    
+    n_corrected = 0
+    
+    
+    mz_keys = np.array(sorted([float(k) for k in residual_signals.keys()]))
+    
+    # Calcula el umbral relativo
+    all_vals = np.concatenate([np.array(v) for v in residual_signals.values()])
+    max_val = np.max(all_vals)
+    intensity_threshold = threshold * max_val
+    print(f"[DEBUG] Threshold = {intensity_threshold:.2e} ({threshold*100:.1f}% of max {max_val:.2e})")
+
+    
+    
+    # Obtengo los mzs e intensidades del file original
+    for i, spectrum in enumerate(input_map):
+        mzs, original_intensities = spectrum.get_peaks()
+        mzs = np.array(mzs)
+        corrected_intensities = np.zeros_like(mzs)
+        #le paso el input_map para poder crear el corrected
+        #print(f"[DEBUG] Spectrum {i} has {len(mzs)} peaks.")
+        #print(f"[DEBUG] First 5 mzs: {mzs[:5]}")
+        #print(f"[DEBUG] First 5 original intensities: {original_intensities[:5]}")
+        
+        for j, mz in enumerate(mzs):
+            idx = np.where(np.abs(mz_keys - mz) <= mz_tol)[0]
+            if idx.size > 0:
+                closest_mz = mz_keys[idx[np.argmin(np.abs(mz_keys[idx] - mz))]]
+                intensity = residual_signals[closest_mz][i]
+
+                if intensity >= intensity_threshold:
+                    corrected_intensities[j] = intensity
+                    n_corrected += 1
+                else:
+                    corrected_intensities[j] = 0  # Elimina la oscilación base
+            else:
+                corrected_intensities[j] = 0  # Fuerza a 0 si no hay corrección posible
+        """for j, mz in enumerate(mzs):
+            # Busca m/z corregida más cercana dentro de la tolerancia
+            idx = np.where(np.abs(mz_keys - mz) <= mz_tol)[0]
+            if idx.size > 0:
+                closest_mz = mz_keys[idx[np.argmin(np.abs(mz_keys[idx] - mz))]]
+                #print(f"[DEBUG] residual_signals[{closest_mz}][{i}] is NaN, skipping correction.")
+                corrected_intensities[j] = residual_signals[closest_mz][i]
+                n_corrected += 1
+                #print(f"[DEBUG] Spectrum {i}, mz {mz:.4f} -> corrected with {closest_mz:.4f}")
+                
+            else:
+                corrected_intensities[j] = original_intensities[j]#si no encuentra esa mz corregida que deje la original
+                print(f"[DEBUG] No match for mz={mz:.5f} in spectrum {i}")
+            """
+        # Reconstruye espectro corregido
+        new_spectrum = oms.MSSpectrum()
+        new_spectrum.set_peaks((mzs, corrected_intensities))
+        new_spectrum.setRT(spectrum.getRT())
+        new_spectrum.setMSLevel(spectrum.getMSLevel())
+        new_spectrum.setDriftTime(spectrum.getDriftTime())
+        new_spectrum.setPrecursors(spectrum.getPrecursors())
+        new_spectrum.setInstrumentSettings(spectrum.getInstrumentSettings())
+        new_spectrum.setAcquisitionInfo(spectrum.getAcquisitionInfo())
+        new_spectrum.setType(spectrum.getType())
+        #print(f"[DEBUG] Spectrum {i} - First 5 corrected intensities: {corrected_intensities[:5]}")
+        corrected_map.addSpectrum(new_spectrum)
+        
+    print(f"[DEBUG] Total intensities replaced: {n_corrected}")
+
+    return corrected_map
 
 def obtain_freq_from_signal(rt_array, mz_array, intensity_array, window_size=70, mz_ref=922.098):
     """
@@ -305,6 +284,34 @@ def obtain_freq_from_signal(rt_array, mz_array, intensity_array, window_size=70,
 
     return local_freqs_ref, phase_ref
 
+def extract_detected_mzs(rt_array, mz_array, intensity_array, intensity_threshold=1.0, decimal_precision=2):
+    """
+    Extracts all mzs with signal 
+
+    Parameters
+    ----------
+    rt_array : TYPE
+        array with rts from the original signal
+    mz_array : list
+        list of arrays of m/z 
+    intensity_array : list
+        list of arrays of intensities
+
+    Returns
+    -------
+    List of mzs with an intensity higher than the treshold established
+
+    """
+    mz_set = set()
+
+    
+    for mzs, intensities in zip(mz_array, intensity_array):#para cada espectro sus mzs y sus intensidades 
+        for mz, intensity in zip(mzs, intensities):#en cada espectro mz e intensidad
+            if intensity >= intensity_threshold:
+                mz_rounded = round(mz, decimal_precision)
+                mz_set.add(mz_rounded)
+
+    return sorted(mz_set)
 
 def process_file(file_path, save_as):
     
@@ -347,32 +354,42 @@ def process_file(file_path, save_as):
     
     
     # 2. Corrijo las oscilaciones
-        #2.1 Genero la onda de ref
-        #2.2 Obtengo la onda de mz2 (sin el pico)
-        #2.3 Le resto a la original (mz2) la generada (mz1) para que se me quede solo el pico
-        
     
-    #Obtengo la frecuencia de la señal de ref para mz=922.098
-    #ref_mz=922.098
+        #2.1 Obtengo la frecuencia de la señal de ref para mz=922.098
+        #ref_mz=922.098
+        
     local_freqs_ref, phase_ref = obtain_freq_from_signal(rts, mz_array, intensity_array)
     
     
-    #Corrijo localmente
+        #2.2 Corrijo localmente
     target_mz_list=[60.07, 65.1, 95.1, 96.085, 110.1173, 922.098]
+    #target_mz_list=extract_detected_mzs(rts, mz_array, intensity_array)
+    
+    auto_mzs=detect_oscillating_mzs(rts, mz_array, intensity_array, target_mz_list, phase_ref, local_freqs_ref)
+    
+    for mz in auto_mzs:
+        if mz not in target_mz_list:
+            target_mz_list.append(mz)
+        
+    cleaned_target_mzs = [float(mz) for mz in target_mz_list]
+    #print(f"TARGET MZS DETECTED: {cleaned_target_mzs}")
+    
+    
     xic_signals = {}
     modulated_signals = {}#Dict[target_mz: float, modulated: np.ndarray]
     residual_signals = {}#Dict[target_mz: float, residual: np.ndarray]
 
     for target_mz in target_mz_list:
-        #xic, residual_signal = apply_gaussian_filter(rts, mz_array, intensity_array, target_mz)
+        float(target_mz)
         xic, modulated_signal, residual_signal=correct_oscillations(rts, mz_array, intensity_array, phase_ref, local_freqs_ref, target_mz)
-        plot_all(rts, target_mz, xic, modulated_signal, residual_signal)
+        #plot_all(rts, target_mz, xic, modulated_signal, residual_signal)
         #plot_original_and_residual(rts, target_mz, xic, residual_signal)
         #plot_modulated_signal(rts, target_mz, modulated_signal)
         xic_signals[target_mz] = xic
         modulated_signals[target_mz] = modulated_signal
         residual_signals[target_mz] = residual_signal
     
+
     
     #[DEBUG] Data exported to csv
     """
@@ -381,15 +398,12 @@ def process_file(file_path, save_as):
     export_tic_signals_to_csv(rts, tic_original, modulated_signal, residual_signal, output_csv_path)
     """
     #FOR EACH MZ
-    output_path="C:/Users/maipa/repos/oscilations_TFG_repository/FILES/MS_files/PRUEBAS_ultimas/XIC_prueba4.csv"
-    export_xic_signals_combined_csv(rts, xic_signals, modulated_signals, residual_signals, output_path)
-    
-    
+    output_path="C:/Users/maipa/repos/oscilations_TFG_repository/FILES/MS_files/PRUEBAS_ultimas/XIC_prueba6.csv"
+    #export_xic_signals_combined_csv(rts, xic_signals, modulated_signals, residual_signals, output_path)
     
     
     # 3. Aplico el cambio a los espectros
-    corrected_map=correct_spectra2(input_map, target_mz_list, rts, residual_signals)
-    #corrected_map=correct_spectra3(input_map, target_mz_list, rts, residual_signals)
+    corrected_map=correct_spectra(input_map, target_mz_list, rts, residual_signals)
     
     
     #Calculo tiempo de ejecución
